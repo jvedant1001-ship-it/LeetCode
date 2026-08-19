52d1631583a90bff93a0c06c6ef470676b475d83
Vedant Joshi <for.work.1871@gmail.com>
2026-08-19 05:37:45 -0800

    Update LeetCode #234 - Palindrome Linked List

--- 0234-palindrome-linked-list/README.md
+++ 0234-palindrome-linked-list/README.md
@@ -4,7 +4,7 @@
 - **Difficulty:** Easy
 - **Language:** Python
 - **Solved:** 2026-08-19
-- **Link:** https://leetcode.com/problems/palindrome-linked-list/
+- **Link:** https://leetcode.com/problems/palindrome-linked-list/submissions/2112739408/
 
 ## Complexity
 
--- 0234-palindrome-linked-list/solution.py
+++ 0234-palindrome-linked-list/solution.py
@@ -1,11 +1,28 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution(object):
+class Solution:
     def isPalindrome(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: bool
-        """
+        # Find the middle
+        slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Reverse the second half
+        prev = None
+        while slow:
+            nxt = slow.next
+            slow.next = prev
+            prev = slow
+            slow = nxt
+
+        # Compare both halves
+        left = head
+        right = prev
+
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+
+        return True
