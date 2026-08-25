124aa31ea41272ad17d641396276d921a013460e
Vedant Joshi <for.work.1871@gmail.com>
2026-08-24 19:57:06 -0800

    Update LeetCode #39 - Combination Sum

--- 0039-combination-sum/README.md
+++ 0039-combination-sum/README.md
@@ -4,7 +4,7 @@
 - **Difficulty:** Medium
 - **Language:** Python
 - **Solved:** 2026-08-24
-- **Link:** https://leetcode.com/problems/combination-sum/description/
+- **Link:** https://leetcode.com/problems/combination-sum/
 
 ## Complexity
 
--- 0039-combination-sum/solution.py
+++ 0039-combination-sum/solution.py
@@ -1,7 +1,25 @@
-class Solution(object):
+class Solution:
     def combinationSum(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
+        result = []
+
+        def backtrack(start, remaining, path):
+            if remaining == 0:
+                result.append(path[:])
+                return
+
+            for i in range(start, len(candidates)):
+                num = candidates[i]
+
+                if num > remaining:
+                    continue
+
+                path.append(num)
+
+                # i (not i + 1) because the same number
+                # can be used unlimited times.
+                backtrack(i, remaining - num, path)
+
+                path.pop()
+
+        backtrack(0, target, [])
+        return result
