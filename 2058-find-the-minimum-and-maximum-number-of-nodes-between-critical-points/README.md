66782763b00141971bf77b9a82c748c4f82f5cc1
Vedant Joshi <for.work.1871@gmail.com>
2026-08-31 03:48:27 -0800

    Update LeetCode #2058 - Find the Minimum and Maximum Number of Nodes Between Critical Points

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/README.md
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/README.md
@@ -4,7 +4,7 @@
 - **Difficulty:** Medium
 - **Language:** Python
 - **Solved:** 2026-08-31
-- **Link:** https://leetcode.com/problems/find-the-minimum-and-maximum-number-of-nodes-between-critical-points/
+- **Link:** https://leetcode.com/problems/find-the-minimum-and-maximum-number-of-nodes-between-critical-points/submissions/2126047824/
 
 ## Complexity
 
--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/solution.py
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/solution.py
@@ -5,7 +5,34 @@
 #         self.next = next
 class Solution(object):
     def nodesBetweenCriticalPoints(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: List[int]
-        """
+            prev = head
+            curr = head.next
+            index = 1
+
+            first = -1
+            last = -1
+            min_dist = float('inf')
+
+            while curr.next:
+                nxt = curr.next
+
+                if (curr.val > prev.val and curr.val > nxt.val) or \
+                (curr.val < prev.val and curr.val < nxt.val):
+
+                    if first == -1:
+                        first = index
+                    else:
+                        min_dist = min(min_dist, index - last)
+
+                    last = index
+
+                prev = curr
+                curr = nxt
+                index += 1
+
+            if first == -1 or first == last:
+                return [-1, -1]
+
+            max_dist = last - first
+
+            return [min_dist, max_dist]
