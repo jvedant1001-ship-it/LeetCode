51a7dd283cec85952bc434bb800ec06762e3376f
Vedant Joshi <for.work.1871@gmail.com>
2026-09-02 06:43:52 -0800

    Update LeetCode #3875 - Construct Uniform Parity Array I

--- 3875-construct-uniform-parity-array-i/README.md
+++ 3875-construct-uniform-parity-array-i/README.md
@@ -4,7 +4,7 @@
 - **Difficulty:** Easy
 - **Language:** Python
 - **Solved:** 2026-09-02
-- **Link:** https://leetcode.com/problems/construct-uniform-parity-array-i/
+- **Link:** https://leetcode.com/problems/construct-uniform-parity-array-i/submissions/2128556838/
 
 ## Complexity
 
--- 3875-construct-uniform-parity-array-i/solution.py
+++ 3875-construct-uniform-parity-array-i/solution.py
@@ -1,6 +1,15 @@
-class Solution(object):
+class Solution:
     def uniformArray(self, nums1):
-        """
-        :type nums1: List[int]
-        :rtype: bool
-        """
+        has_even = False
+        has_odd = False
+
+        for num in nums1:
+            if num % 2 == 0:
+                has_even = True
+            else:
+                has_odd = True
+
+        if not has_even or not has_odd:
+            return True
+
+        return True
