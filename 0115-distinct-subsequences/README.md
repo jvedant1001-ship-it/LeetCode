1999468efeea59a48c0fa763ea9ae76b31978460
Vedant Joshi <for.work.1871@gmail.com>
2026-09-06 07:48:18 -0800

    Update LeetCode #115 - Distinct Subsequences

--- 0115-distinct-subsequences/README.md
+++ 0115-distinct-subsequences/README.md
@@ -4,7 +4,7 @@
 - **Difficulty:** Hard
 - **Language:** Python
 - **Solved:** 2026-09-06
-- **Link:** https://leetcode.com/problems/distinct-subsequences/
+- **Link:** https://leetcode.com/problems/distinct-subsequences/submissions/2133015125/
 
 ## Complexity
 
--- 0115-distinct-subsequences/solution.py
+++ 0115-distinct-subsequences/solution.py
@@ -1,7 +1,17 @@
 class Solution(object):
     def numDistinct(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: int
-        """
+        m, n = len(s), len(t)
+
+        # dp[j] = number of ways to form t[:j]
+        # from the characters processed so far in s
+        dp = [0] * (n + 1)
+        dp[0] = 1
+
+        for i in range(m):
+            # Go backwards so dp[j-1] still represents
+            # the previous row.
+            for j in range(n, 0, -1):
+                if s[i] == t[j - 1]:
+                    dp[j] += dp[j - 1]
+
+        return dp[n]
