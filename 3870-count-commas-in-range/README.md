90e5cecb88cbda8db37ee7fe47bb77694fdde2f5
Vedant Joshi <for.work.1871@gmail.com>
2026-09-08 06:11:39 -0800

    Update LeetCode #3870 - Count Commas in Range

--- 3870-count-commas-in-range/README.md
+++ 3870-count-commas-in-range/README.md
@@ -4,7 +4,7 @@
 - **Difficulty:** Easy
 - **Language:** Python
 - **Solved:** 2026-09-08
-- **Link:** https://leetcode.com/problems/count-commas-in-range/submissions/2135192629/
+- **Link:** https://leetcode.com/problems/count-commas-in-range/
 
 ## Complexity
 
--- 3870-count-commas-in-range/solution.py
+++ 3870-count-commas-in-range/solution.py
@@ -1,14 +1,5 @@
 class Solution:
     def countCommas(self, n):
-        ans = 0
-
-        if n >= 1000:
-            ans += min(n, 9999) - 1000 + 1
-
-        if n >= 10000:
-            ans += (min(n, 99999) - 10000 + 1) * 2
-
-        if n >= 100000:
-            ans += (n - 100000 + 1) * 2
-
-        return ans
+        if n < 1000:
+            return 0
+        return n - 999
