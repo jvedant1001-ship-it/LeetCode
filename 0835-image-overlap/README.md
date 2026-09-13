9929c8fb83a3a8610d01bb0620136d0a8c12d28e
Vedant Joshi <for.work.1871@gmail.com>
2026-09-13 03:48:30 -0800

    Update LeetCode #835 - Image Overlap

--- 0835-image-overlap/README.md
+++ 0835-image-overlap/README.md
@@ -4,7 +4,7 @@
 - **Difficulty:** Medium
 - **Language:** Python
 - **Solved:** 2026-09-13
-- **Link:** https://leetcode.com/problems/image-overlap/
+- **Link:** https://leetcode.com/problems/image-overlap/submissions/2140564086/
 
 ## Complexity
 
--- 0835-image-overlap/solution.py
+++ 0835-image-overlap/solution.py
@@ -1,7 +1,25 @@
-class Solution(object):
+class Solution:
     def largestOverlap(self, img1, img2):
-        """
-        :type img1: List[List[int]]
-        :type img2: List[List[int]]
-        :rtype: int
-        """
+        n = len(img1)
+
+        ones1 = []
+        ones2 = []
+
+        for r in range(n):
+            for c in range(n):
+                if img1[r][c]:
+                    ones1.append((r, c))
+                if img2[r][c]:
+                    ones2.append((r, c))
+
+        shifts = {}
+        ans = 0
+
+        for r1, c1 in ones1:
+            for r2, c2 in ones2:
+                shift = (r2 - r1, c2 - c1)
+
+                shifts[shift] = shifts.get(shift, 0) + 1
+                ans = max(ans, shifts[shift])
+
+        return ans
