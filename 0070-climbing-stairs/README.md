884edbbfd601adcd9a85f689f8760497caa77cba
Vedant Joshi <for.work.1871@gmail.com>
2026-09-21 07:58:28 -0800

    Update LeetCode #70 - Climbing Stairs

--- 0070-climbing-stairs/README.md
+++ 0070-climbing-stairs/README.md
@@ -4,7 +4,7 @@
 - **Difficulty:** Easy
 - **Language:** Python
 - **Solved:** 2026-09-21
-- **Link:** https://leetcode.com/problems/climbing-stairs/
+- **Link:** https://leetcode.com/problems/climbing-stairs/submissions/2148822104/
 
 ## Complexity
 
--- 0070-climbing-stairs/solution.py
+++ 0070-climbing-stairs/solution.py
@@ -1,6 +1,6 @@
-class Solution(object):
+class Solution:
     def climbStairs(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
+        a, b = 0, 1
+        for _ in range(n):
+            a, b = b, a + b
+        return b
