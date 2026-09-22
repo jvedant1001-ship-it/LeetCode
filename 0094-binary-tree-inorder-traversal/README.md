97bb9dd5c53b0006c3e78d121b60bc4c739bb0b5
Vedant Joshi <for.work.1871@gmail.com>
2026-09-22 07:45:57 -0800

    Update LeetCode #94 - Binary Tree Inorder Traversal

--- 0094-binary-tree-inorder-traversal/README.md
+++ 0094-binary-tree-inorder-traversal/README.md
@@ -4,7 +4,7 @@
 - **Difficulty:** Easy
 - **Language:** Python
 - **Solved:** 2026-09-22
-- **Link:** https://leetcode.com/problems/binary-tree-inorder-traversal/
+- **Link:** https://leetcode.com/problems/binary-tree-inorder-traversal/submissions/2149909053/
 
 ## Complexity
 
--- 0094-binary-tree-inorder-traversal/solution.py
+++ 0094-binary-tree-inorder-traversal/solution.py
@@ -4,9 +4,17 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
+class Solution:
     def inorderTraversal(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: List[int]
-        """
+        result = []
+
+        def inorder(node):
+            if not node:
+                return
+
+            inorder(node.left)       # Left
+            result.append(node.val)  # Root
+            inorder(node.right)      # Right
+
+        inorder(root)
+        return result
